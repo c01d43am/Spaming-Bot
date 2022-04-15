3843fcd245b65db839fc2a1e5aefe09a6ed737e9
#2645 <57105127+alugolupawan1@users.noreply.github.com>
2022-04-16 03:43:08 +0530

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,7 +1,7 @@
 import pyautogui, time
-time.sleep(10)
-print("Thanks For Using My Code! Make sure to give it a Star on Github!")
-print("You have 10 seconds to switch to the tab and select the chat where you want to spam the message")
+time.sleep(5)
+print("Thanks For Using My Code! Make sure to give it a Star ")
+print("You have 5 seconds to switch to the tab and select the chat where you want to spam the message")
 file = open("send.txt", 'r')
 for word in file:
     pyautogui.typewrite(word)
